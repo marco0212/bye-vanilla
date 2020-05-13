3b75eb6d532a30203fa1f1a3c371b76ea4fbe871
Jeong <inyeop0212@gmail.com>
2020-05-13 21:26:11 +0900

    Update Style

--- src/App.js
+++ src/App.js
@@ -8,14 +8,23 @@ function App() {
     new Swiper ('.swiper-container', {
       speed: 700,
       spaceBetween: 100,
-      autoplay: {
-        delay: 5000,
-      }
+      // autoplay: {
+      //   delay: 5000,
+      // }
     });
   }, []);
   return (
     <div className="swiper-container">
         <div className="swiper-wrapper">
+        <div
+          className="swiper-slide first"
+          style={{ backgroundImage: `url(https://imgnews.pstatic.net/image/001/2020/05/13/PYH2020051313370001300_P4_20200513175611501.jpg)`}}
+        >
+          <p>
+            여기까지 오게 된 것은 모두 켄님 덕분입니다. Special thanks to ken.<br />
+            2020.01.06 ~ 05.09 바닐라코딩 7기, 스승의 날
+          </p>
+        </div>
           {
             letters.map(letter => {
               const { name, title, text, picture } = letter;
