335d1a85d141ea0a36c134f88c985995ed0419c2
Jeong <inyeop0212@gmail.com>
2020-05-13 21:07:04 +0900

    Update Mark up and Style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import letters from './letters.json';
+import Swiper from 'swiper';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    new Swiper ('.swiper-container', {
+      speed: 700,
+      spaceBetween: 100,
+      autoplay: {
+        delay: 5000,
+      }
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="swiper-container">
+        <div className="swiper-wrapper">
+          {
+            letters.map(letter => {
+              const { name, title, text, picture } = letter;
+              return (
+                <div className="swiper-slide" key={name}>
+                  <div className="content-wrap">
+                    <div className="img-area">
+                      <img src={picture} alt={name}/>
+                    </div>
+                    <div className="text-area">
+                      <h3>{title}</h3>
+                      <p>{text}</p>
+                    </div>
+                  </div>
+                </div>
+              );
+            })
+          }
+      </div>
     </div>
   );
 }
